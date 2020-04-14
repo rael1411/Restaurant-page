09b022fe362eaeb4d41845fd4805806065be45b9
Alessio <barrialex@gmail.com>
2020-04-14 11:06:44 +0200

    default page

---
+++ src/load.js
@@ -0,0 +1,16 @@
+const load = function(){
+    let container = document.getElementById("content");
+    let logo = document.createElement("IMG");
+    let byLine = document.createElement("h1");
+    let description = document.createElement("p");
+    logo.setAttribute("src", "../src/logo.jpg");
+    logo.id = "logo";
+    logo.width = "300";
+    byLine.textContent = "Best restaurant ever";
+    description.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    container.appendChild(logo);
+    container.appendChild(byLine);
+    container.appendChild(description);
+}
+    export { load }
+
